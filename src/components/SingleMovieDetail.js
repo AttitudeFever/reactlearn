496d17ca36aa94f3e3a023b8e63dd93bf1da18d4
mcarv838 <48259924+mcarv838@users.noreply.github.com>
2020-02-18 22:16:54 -0700

    Add files via upload
    SingleMovie details in progress

--- src/components/SingleMovieDetail.js
+++ src/components/SingleMovieDetail.js
@@ -1,11 +1,52 @@
 import React from 'react'
 
-function SingleMovieDetail(props) {
+class SingleMovieDetail extends React.Component {
+
+  constructor(props) {
+     super(props);
+     this.state = {
+       array:[],
+       crew: []
+
+     };
+ }
+
+  componentDidMount()
+  {
+      const map1 = this.props.viewData.production;
+      this.setState(  {array:map1} );
+
+ }
+  render()
+  {
+    // console.log(this.state.crew);
+
+    const imgUrl ="https://image.tmdb.org/t/p/w500"+this.props.viewData.poster;
+    const imdb = "https://www.imdb.com/"+this.props.viewData.title+"/"+this.props.viewData.imdb_id;
+    const tmdb ="https://www.themoviedb.org/movie/"+this.props.viewData.tmdb_id;
     return (
-        <div>
-            <p>{props.viewData.id}</p>
-        </div>
-    )
+
+      <div className="SingleMovieDisplay">
+              <div>
+                 <h3>{this.props.viewData.title}</h3>
+                 <img className="largeImg" src={imgUrl}/>
+               </div>
+
+               <div>
+                   <p>Release Date: {this.props.viewData.release_date} <br/>
+                      Runtime: {this.props.viewData.runtime}<br/>
+                      Revenue: {this.props.viewData.revenue}<br/>
+                      IMDB Link: <a href={imdb}> Click Here</a> <br/>
+                      TMDB Link: <a href={tmdb}> Click Here</a>
+                   </p>
+               </div>
+                  <p>{this.state.array}</p>
+               <div>
+
+               </div>
+      </div>);
+
+}
 }
 
 export default SingleMovieDetail
