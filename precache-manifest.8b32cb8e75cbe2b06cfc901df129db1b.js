ec64f1f1355f7947c3cdf0b6372d907085578fa5
AttitudeFever <36493356+AttitudeFever@users.noreply.github.com>
2020-02-23 16:21:58 -0700

    Updates

---
+++ precache-manifest.8b32cb8e75cbe2b06cfc901df129db1b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dd3975289a93c88e5e9c20af9fd9d5c",
+    "url": "/ReactAppA1/index.html"
+  },
+  {
+    "revision": "ac81cec609ec3b4c7b6b",
+    "url": "/ReactAppA1/static/css/main.48d22bcc.chunk.css"
+  },
+  {
+    "revision": "3bacadc213e92ce06dad",
+    "url": "/ReactAppA1/static/js/2.05225ba5.chunk.js"
+  },
+  {
+    "revision": "7f31737ae9519616291bf043b5e7c506",
+    "url": "/ReactAppA1/static/js/2.05225ba5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ac81cec609ec3b4c7b6b",
+    "url": "/ReactAppA1/static/js/main.6e7a5740.chunk.js"
+  },
+  {
+    "revision": "b98ece3762099dca1a55",
+    "url": "/ReactAppA1/static/js/runtime-main.ce61690c.js"
+  },
+  {
+    "revision": "930b17339d75c2a712d4cfaa8ba1dfd0",
+    "url": "/ReactAppA1/static/media/bgAboutus.930b1733.jpg"
+  },
+  {
+    "revision": "aa85a6fd5d5aca3f0f7e650b5411eaf8",
+    "url": "/ReactAppA1/static/media/hamid.aa85a6fd.png"
+  },
+  {
+    "revision": "7a1fd145c2f8dc3a2c74ecdee3ab906d",
+    "url": "/ReactAppA1/static/media/loader2.7a1fd145.gif"
+  },
+  {
+    "revision": "ac49e172dd6968a57a0e5c03016a1f8c",
+    "url": "/ReactAppA1/static/media/logo2.ac49e172.png"
+  },
+  {
+    "revision": "0356f72b7151637cd027a55747260074",
+    "url": "/ReactAppA1/static/media/mark.0356f72b.jpg"
+  },
+  {
+    "revision": "fda470f9398a91bed7fe6a356c7851a6",
+    "url": "/ReactAppA1/static/media/unknown_cast.fda470f9.jpg"
+  }
+]);
